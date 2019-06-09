1c27132fd1dcfda54811db7940cdcd5f0cc0feb5
Chirag Mogra <43244064+chiragm24@users.noreply.github.com>
2019-06-09 21:01:19 +0000

    Add files via upload

---
+++ prob5.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+import time
+name =input("Enter Your Name ")
+curr_tm_hour=int(time.localtime().tm_hour)
+print(curr_tm_hour)
+if curr_tm_hour>=5 and curr_tm_hour<12:
+	print("Good Morning",name)
+elif curr_tm_hour>=12 and curr_tm_hour<17:
+        print("Good Afternoon",name)
+elif curr_tm_hour>=17 and curr_tm_hour<21:
+        print("Good Evening",name)
+elif curr_tm_hour>=21 or curr_tm_hour<5:
+        print("Good Night",name)
+
