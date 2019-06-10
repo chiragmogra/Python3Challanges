c00339f4855eeaedb68d08e78d86cc85e7c6c1c3
Chirag Mogra <43244064+chiragm24@users.noreply.github.com>
2019-06-10 17:53:24 +0000

    Add files via upload

---
+++ prob4.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+import os
+import subprocess
+uno=int(input("please enter no of users"))
+ulist=[]
+i=0
+while i<uno:
+	uname=input("Enter User Name ")
+	if uname.isalpha():
+		print("uname is valid")
+		ulist.append(uname)
+		i+=1
+	else:
+		print("uname is not valid(It does not consist numbers)")
+
+for i in ulist:
+	pas="hello"+i
+	comf="useradd -p $(openssl passwd -1 "+pas+") "+i
+	os.system(comf)
+	print(uname," user added ")
+
