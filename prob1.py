091ec74260000442f49e69e6046e9899ee85158a
Chirag Mogra <43244064+chiragm24@users.noreply.github.com>
2019-06-10 17:57:16 +0000

    Update prob1.py
    Problem 1 :
    
     Create a program that asks the user to enter their name and their age.
     Print out a message that will tell them the year that they will turn 95 years old.

--- prob1.py
+++ prob1.py
@@ -4,4 +4,4 @@ name=input("Enter Your Name ")
 age=int(input("Enter Your Age "))
 curr_year=(datetime.datetime.now()).year
 year=95+curr_year-age
-print("When you turn your age is 95 than that year will be ",year)
+print("When your age is 95 than that year will be ",year)
