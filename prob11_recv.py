84213285cc03023554492c621fdfae92f3b2eefc
Chirag Mogra <43244064+chiragmogra@users.noreply.github.com>
2019-06-14 22:16:02 +0530

    Update prob11_recv.py

--- prob11_recv.py
+++ prob11_recv.py
@@ -1,14 +1,11 @@
 #!usr/bin/python2
 import socket
 recv_ip="127.0.0.1"
-recv_port=4444 # 0-1024 -- you can check free udp port netstat -nulp
-#create udp
-#    ip type v4,v6
+recv_port=4444 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-# fitting ip and port with udp socket
 s.bind((recv_ip,recv_port))
 while True:
-# receive data from sender
+
 	data=s.recvfrom(150)
 	if data[0]=="###":
 		break
